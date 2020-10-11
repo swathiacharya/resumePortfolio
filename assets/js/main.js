b72a43290e7d3268f187b506b88e9f49759b10ef
swathiacharya <acharyaswathi999@gmail.com>
2020-10-11 15:11:10 +0530

    added animation for error

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,3 @@ sr.reveal('.work__img',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
-
-sr.reveal('.skills__sub',{interval:200});
-sr.reveal('.skills__textval',{interval:200});
